aae880441575331ec377fbbadb6bd53ae2299b5d
Kai-Abrorbek <abror0dev@gmail.com>
2025-10-24 11:37:22 +0900

    fix: modify TopAgents and TopProperties sectional components

--- libs/components/homepage/TopAgents.tsx
+++ libs/components/homepage/TopAgents.tsx
@@ -8,6 +8,9 @@ import { Autoplay, Navigation, Pagination } from 'swiper';
 import TopAgentCard from './TopAgentCard';
 import { Member } from '../../types/member/member';
 import { AgentsInquiry } from '../../types/member/member.input';
+import { GET_AGENTS } from '../../../apollo/user/query';
+import { T } from '../../types/common';
+import { useQuery } from '@apollo/client';
 
 interface TopAgentsProps {
 	initialInput: AgentsInquiry;
@@ -20,6 +23,20 @@ const TopAgents = (props: TopAgentsProps) => {
 	const [topAgents, setTopAgents] = useState<Member[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getAgentsLoading,
+		data: getAgentsData,
+		error: getAgentsError,
+		refetch: getAgentsRefetch,
+	} = useQuery(GET_AGENTS, {
+		fetchPolicy: 'cache-and-network',
+		variables: { input: initialInput },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			console.log(data);
+			setTopAgents(data?.getAgents?.list);
+		},
+	});
 	/** HANDLERS **/
 
 	if (device === 'mobile') {
@@ -102,7 +119,7 @@ const TopAgents = (props: TopAgentsProps) => {
 TopAgents.defaultProps = {
 	initialInput: {
 		page: 1,
-		limit: 10,
+		limit: 5,
 		sort: 'memberRank',
 		direction: 'DESC',
 		search: {},
--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -37,7 +37,7 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 	});
 	/** HANDLERS **/
 
-	if (trendProperties) console.log('trendProperties: ====', trendProperties);
+	// if (trendProperties) console.log('trendProperties: ====', trendProperties);
 	if (!trendProperties) return null;
 
 	if (device === 'mobile') {

757601537aedcd782ceb6021a54a1fbb2402a811
Kai-Abrorbek <abror0dev@gmail.com>
2025-10-24 11:48:36 +0900

    fix: modify ComunityBoards component

--- libs/components/homepage/CommunityBoards.tsx
+++ libs/components/homepage/CommunityBoards.tsx
@@ -4,6 +4,11 @@ import useDeviceDetect from '../../hooks/useDeviceDetect';
 import { Stack, Typography } from '@mui/material';
 import CommunityCard from './CommunityCard';
 import { BoardArticle } from '../../types/board-article/board-article';
+import { useQuery } from '@apollo/client';
+import { GET_BOARD_ARTICLES } from '../../../apollo/user/query';
+import { T } from '../../types/common';
+import { AllBoardArticlesInquiry } from '../../types/board-article/board-article.input';
+import { BoardArticleCategory } from '../../enums/board-article.enum';
 
 const CommunityBoards = () => {
 	const device = useDeviceDetect();
@@ -16,6 +21,33 @@ const CommunityBoards = () => {
 	const [freeArticles, setFreeArticles] = useState<BoardArticle[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getNewsBoardArticlesLoading,
+		data: getNewsBoardArticlesData,
+		error: getNewsBoardArticlesError,
+		refetch: getNewsBoardArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only',
+		variables: { input: { ...searchCommunity, limit: 6, search: { articleCategory: BoardArticleCategory.NEWS } } },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setNewsArticles(data?.getBoardArticles?.list);
+		},
+	});
+
+	const {
+		loading: getFreeBoardArticlesLoading,
+		data: getFreeBoardArticlesData,
+		error: getFreeBoardArticlesError,
+		refetch: getFreeBoardArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only',
+		variables: { input: { ...searchCommunity, limit: 6, search: { articleCategory: BoardArticleCategory.FREE } } },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setFreeArticles(data?.getBoardArticles?.list);
+		},
+	});
 
 	if (device === 'mobile') {
 		return <div>COMMUNITY BOARDS (MOBILE)</div>;

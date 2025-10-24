cfe8822620143830f5a265f2b5f7714dd52e6653
Kai-Abrorbek <abror0dev@gmail.com>
2025-10-24 10:43:56 +0900

    feat: develop TrendProperties component based on react query data

--- apollo/client.ts
+++ apollo/client.ts
@@ -44,7 +44,6 @@ function createIsomorphicLink() {
 		const link = new createUploadLink({
 			uri: process.env.REACT_APP_API_GRAPHQL_URL,
 		});
-
 		/* WEBSOCKET SUBSCRIPTION LINK */
 		const wsLink = new WebSocketLink({
 			uri: process.env.REACT_APP_API_WS ?? 'ws://127.0.0.1:3007',
